95016d7ca9aeae5bd9c119a6a6722c489f35d0e0
Fabrício Cintra <fabricioalvescintra@gmail.com>
2023-02-24 23:40:46 -0300

    Midi

---
+++ main.js
@@ -0,0 +1,37 @@
+    function tocaSom(seletorAudio){
+          const elemento = document.querySelector(seletorAudio);
+          if(elemento && elemento.localName === 'audio'){
+                elemento.play();           
+          }else{
+            console.log("erro")
+          }
+        
+    };
+
+    const listButtons = document.querySelectorAll(".tecla");
+    
+    for(let a = 0; a < listButtons.length; a++){
+
+        const tecla = listButtons[a];//0 1 2...
+        const instrumento = tecla.classList[1];//nome da variavel
+        const idAudio = `#som_${instrumento}`;//criando uma string 
+
+       tecla.onclick = function(e){//quando clicar na tecla
+        tocaSom(idAudio);//funcao tocasom recebe id audio.
+       }
+       
+    tecla.onkeydown = function(e){
+
+        if(e.code === 'Space' || e.code ==='Enter'){
+            tecla.classList.add("ativa");
+        }
+        
+            tecla.onkeyup = function(){
+                tecla.classList.remove("ativa");      
+            }
+    }
+       
+       
+    };
+
+       
